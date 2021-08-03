e86df8590325c818d4f1cee14ff067702c1ab1a9
Harut <hro.petros@mail.ru>
2021-08-04 00:34:38 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const numberOfFilms = +prompt("Vorqan film eq duq ditel, minch ayjm", '');
+
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
+
+
+const a = prompt("Verjin ditac filmeric mky?", ''),
+      b = prompt("Inchqan kgnahateiq fulmy?", ''),
+      c = prompt("Verjin ditac filmeric mky?", ''),
+      d = prompt("Inchqan kgnahateiq fulmy?", '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
+
+
+
+     
+     // if (num < 46) {
+     //       console.log("sxal e");
+     // }else if (num > 70){
+     //       console.log('sxal e');
+     // }else if (num == 50){
+     //       console.log('jisht e');
+     // }
+
+
+      const num = (50);
+        switch (num){
+            case 49:
+                console.log('');
+
+        }
+
+
